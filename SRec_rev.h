09f32dd8826a37418a9e90c123727167663cba2e
Fredrik Wikstrom <fredrik@a500.org>
2016-10-14 21:53:33 +0300

    Bumped version to 2.4.

--- SRec_rev.h
+++ SRec_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		2
-#define REVISION	3
-#define DATE		"12.10.2016"
-#define VERS		"SRec 2.3"
-#define VSTRING		"SRec 2.3 (12.10.2016)\r\n"
-#define VERSTAG		"\0$VER: SRec 2.3 (12.10.2016)"
+#define REVISION	4
+#define DATE		"14.10.2016"
+#define VERS		"SRec 2.4"
+#define VSTRING		"SRec 2.4 (14.10.2016)\r\n"
+#define VERSTAG		"\0$VER: SRec 2.4 (14.10.2016)"
