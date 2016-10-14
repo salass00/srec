56d12ee794f76564804c1612277bc37442503c05
Fredrik Wikstrom <fredrik@a500.org>
2016-10-14 18:27:27 +0300

    Added software scale function which will be used for CLUT bitmaps.

---
+++ src/scale.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2016 Fredrik Wikstrom <fredrik@a500.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "scale.h"
+#include "srec_internal.h"
+#include <sys/param.h>
+#include <string.h>
+#include <math.h>
+
+#define CLAMP(val,min,max) MAX(MIN(val, max), min)
+
+void scale_bitmap(struct SRecGlobal *gd, struct BitMap *src_bm, struct BitMap *dst_bm,
+	uint32 src_x, uint32 src_y, uint32 src_w, uint32 src_h,
+	uint32 dst_x, uint32 dst_y, uint32 dst_w, uint32 dst_h,
+	float scale_x, float scale_y)
+{
+	struct GraphicsIFace *IGraphics = gd->igraphics;
+	APTR src_lock, dst_lock;
+	uint32 src_bpr, dst_bpr;
+	uint32 bpp;
+	const uint8 *src, *src_row;
+	uint8 *dst;
+	uint32 dst_mod;
+
+	bpp = IGraphics->GetBitMapAttr(src_bm, BMA_BYTESPERPIXEL);
+
+	src_lock = IGraphics->LockBitMapTags(src_bm,
+		LBM_BaseAddress, &src,
+		LBM_BytesPerRow, &src_bpr,
+		TAG_END);
+	dst_lock = IGraphics->LockBitMapTags(dst_bm,
+		LBM_BaseAddress, &dst,
+		LBM_BytesPerRow, &dst_bpr,
+		TAG_END);
+
+	src += (src_y * src_bpr) + (src_x * bpp);
+	dst += (dst_y * dst_bpr) + (dst_x * bpp);
+
+	dst_mod = dst_bpr - (dst_w * bpp);
+
+	switch (bpp) {
+		case 1:
+			for (dst_y = 0; dst_y != dst_h; dst_y++) {
+				src_y = CLAMP(lroundf((float)dst_y * scale_y), 0, src_h - 1);
+				src_row = src + (src_y * src_bpr);
+				for (dst_x = 0; dst_x != dst_w; dst_x++) {
+					src_x = CLAMP(lroundf((float)dst_x * scale_x), 0, src_w - 1);
+					*dst++ = *(src_row + src_x);
+				}
+				dst += dst_mod;
+			}
+			break;
+
+		default:
+			for (dst_y = 0; dst_y != dst_h; dst_y++) {
+				src_y = CLAMP(lroundf((float)dst_y * scale_y), 0, src_h - 1);
+				src_row = src + (src_y * src_bpr);
+				for (dst_x = 0; dst_x != dst_w; dst_x++) {
+					src_x = CLAMP(lroundf((float)dst_x * scale_x), 0, src_w - 1);
+					memcpy(dst, src_row + (src_x * bpp), bpp);
+					dst += bpp;
+				}
+				dst += dst_mod;
+			}
+			break;
+	}
+
+	if (src_lock != NULL)
+		IGraphics->UnlockBitMap(src_lock);
+	if (dst_lock != NULL)
+		IGraphics->UnlockBitMap(dst_lock);
+}
+
