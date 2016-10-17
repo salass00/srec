f0d96570fa930bdb80b40b5ba263e16bb3296d4b
Fredrik Wikstrom <fredrik@a500.org>
2016-10-17 20:32:52 +0300

    Removed the altivec prefetching as it not only was extra work to maintain but actually made the code slower.

--- src/zmbv_altivec.c
+++ src/zmbv_altivec.c
@@ -21,8 +21,6 @@
 
 typedef __vector uint8 vuint8;
 
-#define PREFETCH 1
-
 static inline vuint8 zmbv_xor_row_altivec(const struct zmbv_state *state, uint8 *out,
 	const uint8 *row1, const uint8 *row2, uint32 row_len)
 {
@@ -108,35 +106,13 @@ static inline vuint8 zmbv_xor_row_altivec_unaligned(const struct zmbv_state *sta
 	return result;
 }
 
-#ifdef PREFETCH
-static inline uint32 get_prefetch_constant(uint32 block_size_in_vectors, uint32 block_count, uint32 block_stride) {
-	return ((block_size_in_vectors << 24) & 0x1f000000) |
-	       ((block_count << 16) & 0x00ff0000) |
-	       (block_stride & 0xffff);
-}
-#endif
-
 #ifdef ENABLE_CLUT
 uint8 zmbv_xor_palette_altivec(const struct zmbv_state *state,
 	const uint8 *src, uint8 *dst)
 {
-	vuint8  result = vec_splat_u8(0);
-	#ifdef PREFETCH
-	uint32  prefetch;
-	#endif
-
-	#ifdef PREFETCH
-	prefetch = get_prefetch_constant(1, (3 * 256) / 16, 16);
-	vec_dst(src, prefetch, 0);
-	vec_dstst(dst, prefetch, 1);
-	#endif
+	vuint8 result;
 
 	result = zmbv_xor_row_altivec(state, dst, src, dst, 3 * 256);
-
-	#ifdef PREFETCH
-	vec_dss(0);
-	vec_dss(1);
-	#endif
 
 	return vec_all_eq(result, vec_splat_u8(0)) ? 0 : 1;
 }
@@ -147,18 +123,9 @@ uint8 zmbv_xor_block_altivec(const struct zmbv_state *state,
 	uint32 bpr, uint8 **outp)
 {
 	vuint8  result = vec_splat_u8(0);
-	#ifdef PREFETCH
-	uint32  prefetch;
-	#endif
 	uint8  *out = *outp;
 	uint32  i;
 	vuint8  x;
-
-	#ifdef PREFETCH
-	prefetch = get_prefetch_constant((blk_w + 15) >> 4, blk_h, bpr);
-	vec_dst(ras1, prefetch, 0);
-	vec_dst(ras2, prefetch, 1);
-	#endif
 
 	for (i = 0; i != blk_h; i++) {
 		if (((uint32)out & 15) == 0)
@@ -173,41 +140,17 @@ uint8 zmbv_xor_block_altivec(const struct zmbv_state *state,
 		out  += blk_w;
 	}
 
-	#ifdef PREFETCH
-	vec_dss(0);
-	vec_dss(1);
-	#endif
-
-	if (!vec_all_eq(result, vec_splat_u8(0))) {
-		*outp = out;
-		return 1;
-	} else {
+	if (vec_all_eq(result, vec_splat_u8(0)))
 		return 0;
-	}
+
+	*outp = out;
+	return 1;
 }
 
 static const vuint8 endian_swap_32bit = { 3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12 };
 static const vuint8 endian_swap_16bit = { 1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14 };
 static const vuint8 rgba32_to_bgra32  = { 2, 1, 0, 3, 6, 5, 4, 7, 10, 9, 8, 11, 14, 13, 12, 15 };
 static const vuint8 abgr32_to_bgra32  = { 1, 2, 3, 0, 5, 6, 7, 4, 9, 10, 11, 8, 13, 14, 15, 12 };
-
-#ifdef PREFETCH
-#define MAX_VECTORS (32UL * 256UL)
-
-static inline uint32 get_prefetch_constant_simple(uint32 total_vectors) {
-	uint32 block_size_in_vectors = 1;
-	uint32 block_count           = total_vectors;
-	uint32 block_stride          = 16;
-
-	while (block_count > 256) {
-		block_size_in_vectors <<= 1;
-		block_stride          <<= 1;
-		block_count             = (block_count + 1) >> 1;
-	}
-
-	return get_prefetch_constant(block_size_in_vectors, block_count, block_stride);
-}
-#endif
 
 void zmbv_format_convert_altivec(const struct zmbv_state *state,
 	const uint8 *src, uint8 *dst, uint32 packed_bpr, uint32 height,
@@ -216,11 +159,6 @@ void zmbv_format_convert_altivec(const struct zmbv_state *state,
 	vuint8 perm_vector;
 	uint32 vectors;
 	uint32 mod;
-	#ifdef PREFETCH
-	uint32 max_prefetch;
-	uint32 prefetch;
-	uint32 num_max, k;
-	#endif
 	uint32 i, j;
 	vuint8 x, y;
 
@@ -247,41 +185,8 @@ void zmbv_format_convert_altivec(const struct zmbv_state *state,
 	vectors = (packed_bpr + 15) >> 4;
 	mod = (padded_bpr - packed_bpr) & ~15;
 
-	#ifdef PREFETCH
-	max_prefetch = get_prefetch_constant(32, 256, 512);
-	prefetch     = get_prefetch_constant_simple(vectors);
-
-	num_max = vectors / MAX_VECTORS;
-	vectors = vectors % MAX_VECTORS;
-	#endif
-
 	for (i = 0; i != height; i++) {
-		#ifdef PREFETCH
-		for (j = 0; j != num_max; j++) {
-			if (src == dst)
-				vec_dstst(src, max_prefetch, 0);
-			else
-				vec_dst(src, max_prefetch, 0);
-
-			for (k = 0; k != MAX_VECTORS; k++) {
-				x = vec_ld(0, src);
-				y = vec_perm(x, x, perm_vector);
-				vec_st(y, 0, dst);
-
-				src += 16;
-				dst += 16;
-			}
-		}
-		#endif
-
 		if (vectors != 0) {
-			#ifdef PREFETCH
-			if (src == dst)
-				vec_dstst(src, prefetch, 0);
-			else
-				vec_dst(src, prefetch, 0);
-			#endif
-
 			for (j = 0; j != vectors; j++) {
 				x = vec_ld(0, src);
 				y = vec_perm(x, x, perm_vector);
@@ -295,9 +200,5 @@ void zmbv_format_convert_altivec(const struct zmbv_state *state,
 		src += mod;
 		dst += mod;
 	}
-
-	#ifdef PREFETCH
-	vec_dss(0);
-	#endif
 }
 
