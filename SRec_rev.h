bac973e4970c204eb02bd596ff7a1b3d7dd081e5
Fredrik Wikstrom <fredrik@a500.org>
2016-11-16 13:22:43 +0200

    Bumped version to 2.6.

--- SRec_rev.h
+++ SRec_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		2
-#define REVISION	5
-#define DATE		"17.10.2016"
-#define VERS		"SRec 2.5"
-#define VSTRING		"SRec 2.5 (17.10.2016)\r\n"
-#define VERSTAG		"\0$VER: SRec 2.5 (17.10.2016)"
+#define REVISION	6
+#define DATE		"16.11.2016"
+#define VERS		"SRec 2.6"
+#define VSTRING		"SRec 2.6 (16.11.2016)\r\n"
+#define VERSTAG		"\0$VER: SRec 2.6 (16.11.2016)"
